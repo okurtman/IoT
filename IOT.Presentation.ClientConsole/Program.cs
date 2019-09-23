b7eba6b3af0609b7b4176529eadff77f23f06cf4
okurtman <okurtman@gmail.com>
2019-09-23 14:19:11 +0300

    Add files via upload

---
+++ IOT.Presentation.ClientConsole/Program.cs
@@ -0,0 +1,29 @@
+﻿using IOT.Domain.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IOT.Presentation.ClientConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new IOTClient(1, "localhost", 1234);
+
+            client.Connect();
+
+            while (true)
+            {
+                var cmd = Console.ReadLine();
+                if (cmd.Equals("exit"))
+                {
+                    client.Disconnect();
+                    break;
+                }
+            }
+        }
+    }
+}
